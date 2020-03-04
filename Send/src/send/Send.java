99b03eb1cb678c2b88225d2dd1c23cda96796484
Itzmar Franco <kairifranco@rocketmail.com>
2020-03-03 23:09:39 -0600

    Add files via upload

---
+++ Send/src/send/Send.java
@@ -0,0 +1,17 @@
+package send;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.nio.charset.StandardCharsets;
+
+public class Send {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] argv) throws Exception {
+        ComprarProducto ventana = new ComprarProducto();
+        ventana.show();
+    }
+}
